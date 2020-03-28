492164db6960968aa04b46d9c8a0ddcc27a80300
Anil Kumar <anil.kumar@rategain.com>
2020-03-28 17:19:53 +0530

    activemq example commit

---
+++ spring-boot-activemq/src/main/java/com/akgcloud/activemq/springbootactivemq/Consumer.java
@@ -0,0 +1,33 @@
+package com.akgcloud.activemq.springbootactivemq;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer{
+
+	@JmsListener(destination = "test.queue")
+	public void consume(String message) throws InterruptedException {
+		Thread.sleep(2000);
+		System.out.println("Message received : " + message);
+	}
+
+//	@Override
+//	public void onMessage(Message message) {
+//		try {
+//			Thread.sleep(10000);
+//			TextMessage msg = (TextMessage) message;
+//			System.out.println("Message Recieved : " + msg.getText());
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
+
+}
---
+++ spring-boot-activemq/src/test/java/com/akgcloud/activemq/springbootactivemq/SpringBootActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akgcloud.activemq.springbootactivemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActivemqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
