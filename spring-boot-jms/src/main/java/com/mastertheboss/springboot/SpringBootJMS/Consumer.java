458463313d6001935ea346f15bcf5c173239827b
Anil Kumar <anil.kumar@rategain.com>
2020-03-28 17:26:43 +0530

    spring boot jms example commit

---
+++ spring-boot-jms/src/main/java/com/mastertheboss/springboot/SpringBootJMS/Consumer.java
@@ -0,0 +1,27 @@
+package com.mastertheboss.springboot.SpringBootJMS;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Consumer implements MessageListener {
+	private Logger log = LoggerFactory.getLogger(Consumer.class);
+
+	@Override
+	public void onMessage(Message message) {
+		try {
+			if (message instanceof TextMessage) {
+				TextMessage msg = (TextMessage) message;
+				log.info("Received text message: " + msg.getText());
+			} else {
+				log.info("Received customer message: " + message.getBody(Customer.class));
+			}
+		} catch (JMSException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
